427a451d367e65d89e5268739a28af8f832cca85
anon <122219518+avocado-beans@users.noreply.github.com>
2024-08-25 16:35:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import threading
 import uuid
 
 Games = ['Riding Extreme 3D','My Clone Army','Chain Cube 2048','Train Miner','Merge Away','Twerk Race 3D','Polysphere','Mud Racing','Mow and Trim']
-hide_streamlit_style = """<style>div[data-testid="stToolbar"] {visibility: hidden;height: 0%;position: fixed;}div[data-testid="stDecoration"] {visibility: hidden;height: 0%;position: fixed;}div[data-testid="stStatusWidget"] {visibility: hidden;height: 0%;position: fixed;}#MainMenu {visibility: hidden;height: 0%;}header {visibility: hidden;height: 0%;}footer {visibility: hidden;height: 0%;}</style>"""
+hide_streamlit_style = """<script src="https://telegram.org/js/telegram-web-app.js"></script><style>div[data-testid="stToolbar"] {visibility: hidden;height: 0%;position: fixed;}div[data-testid="stDecoration"] {visibility: hidden;height: 0%;position: fixed;}div[data-testid="stStatusWidget"] {visibility: hidden;height: 0%;position: fixed;}#MainMenu {visibility: hidden;height: 0%;}header {visibility: hidden;height: 0%;}footer {visibility: hidden;height: 0%;}</style>"""
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 def generate_code():
